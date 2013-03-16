0276d258492acf4add09f9f9621ba349ed18df49
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2013-03-16 06:09:58 +0000

    Add setter for currentTransform (but neither currentTransformInverse nor the 'moz' prefixed versions). Also wrap resetTransform method. Both of these changes bring this polyfill closer to the HTML5 Canvas API as described in the latest specification thereof at time of writing.
    git-svn-id: https://svn/svn/canvas.currentTransform.js@2752 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- canvas.currentTransform.js
+++ canvas.currentTransform.js
@@ -1,5 +1,6 @@
 (function() {
-	var canvas = document.createElement('canvas'),
+	var initialMatrix = [1, 0, 0, 1, 0, 0],
+		canvas = document.createElement('canvas'),
 		context = canvas.getContext("2d"),
 		canvasProto,
 		contextProto,
@@ -18,6 +19,10 @@
 			get: function() {
 				return this.mozCurrentTransform;
 			},
+			set: function(newMatrix) {
+				this.mozCurrentTransform = newMatrix;
+				return newMatrix;
+			},
 			configurable: false,
 			enumerable: false
 		});
@@ -27,6 +32,7 @@
 			get: function() {
 				return this.mozCurrentTransformInverse;
 			},
+			// No setter for inverse matrix
 			configurable: false,
 			enumerable: false
 		});
@@ -43,6 +49,7 @@
 		originalTranslate = contextProto.translate;
 		originalTransform = contextProto.transform;
 		originalSetTransform = context.setTransform;
+		originalResetTransform = contextProto.resetTransform;
 		// Over-ride the Canvas factory method that creates Contexts
 		canvasProto.getContext = function() {
 			// Workaround: older browsers do not accept 'arguments' as second parameter of Function.apply
@@ -51,7 +58,7 @@
 			Object.defineProperty(context, '_transformMatrix', {
 				configurable: false,
 				enumerable: false,
-				value: [1, 0, 0, 1, 0, 0],
+				value: initialMatrix,
 				writable: true
 			});
 			Object.defineProperty(context, '_transformStack', {
@@ -111,6 +118,10 @@
 		};
 		contextProto.setTransform = function(a, b, c, d, e, f) {
 			this._transformMatrix = [ a, b, c, d, e, f];
+			return originalSetTransform.apply(this, Array.prototype.slice.call(arguments));
+		};
+		contextProto.resetTransform = function() {
+			this._transformMatrix = initialMatrix;
 			return originalSetTransform.apply(this, Array.prototype.slice.call(arguments));
 		};
 	}
