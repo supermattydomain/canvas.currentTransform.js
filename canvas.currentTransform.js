a71d23a197e3ec8d31e92e656998546595aefa07
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2013-03-16 03:37:02 +0000

    Assume project is in 'lib' subdir, and adjust paths accordingly. Be more careful/flexible about finding the prototypes for Canvas and its 2D context.
    git-svn-id: https://svn/svn/canvas.currentTransform.js@2749 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- canvas.currentTransform.js
+++ canvas.currentTransform.js
@@ -1,22 +1,33 @@
 (function() {
 	var canvas = document.createElement('canvas'),
 		context = canvas.getContext("2d"),
-		needPolyfill = !('mozCurrentTransform' in context.__proto__),
-		needPolyfillInverse = !('mozCurrentTransformInverse' in context.__proto__),
+		canvasProto,
+		contextProto,
+		needPolyfill,
+		needPolyfillInverse,
 		originalGetContext, originalSave, originalRestore, originalRotate,
 		originalScale, originalTranslate, originalTransform;
+	if (Object.getPrototypeOf) {
+		canvasProto = Object.getPrototypeOf(canvas);
+		contextProto = Object.getPrototypeOf(context);
+	} else {
+		canvasProto = canvas.__proto__ || canvas.constructor.prototype || HTMLCanvasElement;
+		contextProto = context.__proto__ || context.constructor.prototype || CanvasRenderingContext2D;
+	}
+	needPolyfill = !('mozCurrentTransform' in contextProto);
+	needPolyfillInverse = !('mozCurrentTransformInverse' in contextProto);
 	if (needPolyfill || needPolyfillInverse) {
 		// Saved values of over-ridden Canvas methods
-		originalGetContext = canvas.__proto__.getContext;
+		originalGetContext = canvasProto.getContext;
 		// Saved values of over-ridden Context methods
-		originalSave      = context.__proto__.save;
-		originalRestore   = context.__proto__.restore;
-		originalRotate    = context.__proto__.rotate;
-		originalScale     = context.__proto__.scale;
-		originalTranslate = context.__proto__.translate;
-		originalTransform = context.__proto__.transform;
+		originalSave      = contextProto.save;
+		originalRestore   = contextProto.restore;
+		originalRotate    = contextProto.rotate;
+		originalScale     = contextProto.scale;
+		originalTranslate = contextProto.translate;
+		originalTransform = contextProto.transform;
 		// Over-ride the Canvas factory method that creates Contexts
-		canvas.__proto__.getContext = function() {
+		canvasProto.getContext = function() {
 			// Workaround: older browsers do not accept 'arguments' as second parameter of Function.apply
 			var context = originalGetContext.apply(this, Array.prototype.slice.call(arguments));
 			// Using Object.defineProperty rather than simple assignment in order to hide these 'private' data
@@ -35,30 +46,30 @@
 			return context;
 		};
 		// Over-ride each Context method that modifies the transform matrix
-		context.__proto__.save = function() {
+		contextProto.save = function() {
 			this._transformStack.push(this._transformMatrix.concat()); // shallow copy
 			return originalSave.apply(this, Array.prototype.slice.call(arguments));
 		};
-		context.__proto__.restore = function() {
+		contextProto.restore = function() {
 			var mtx = this._transformStack.pop();
 			if (mtx) {
 				this._transformMatrix = mtx;
 			}
 			return originalRestore.apply(this, Array.prototype.slice.call(arguments));
 		};
-		context.__proto__.translate = function(x, y) {
+		contextProto.translate = function(x, y) {
 			this._transformMatrix[4] += this._transformMatrix[0] * x + this._transformMatrix[2] * y;
 			this._transformMatrix[5] += this._transformMatrix[1] * x + this._transformMatrix[3] * y;
 			return originalTranslate.apply(this, Array.prototype.slice.call(arguments));
 		};
-		context.__proto__.scale = function(x, y) {
+		contextProto.scale = function(x, y) {
 			this._transformMatrix[0] *= x;
 			this._transformMatrix[1] *= x;
 			this._transformMatrix[2] *= y;
 			this._transformMatrix[3] *= y;
 			return originalScale.apply(this, Array.prototype.slice.call(arguments));
 		};
-		context.__proto__.transform = function(a, b, c, d, e, f) {
+		contextProto.transform = function(a, b, c, d, e, f) {
 			this._transformMatrix = [
 				this._transformMatrix[0] * a + this._transformMatrix[2] * b,
 				this._transformMatrix[1] * a + this._transformMatrix[3] * b,
@@ -69,11 +80,11 @@
 			];
 			return originalTransform.apply(this, Array.prototype.slice.call(arguments));
 		};
-		context.__proto__.rotate = function(angle) {
+		contextProto.rotate = function(angle) {
 			var c = Math.cos(angle), s = Math.sin(angle);
 			this._transformMatrix = [
-				this._transformMatrix[0] * c + this._transformMatrix[2] * s,
-				this._transformMatrix[1] * c + this._transformMatrix[3] * s,
+				this._transformMatrix[0] *  c + this._transformMatrix[2] * s,
+				this._transformMatrix[1] *  c + this._transformMatrix[3] * s,
 				this._transformMatrix[0] * -s + this._transformMatrix[2] * c,
 				this._transformMatrix[1] * -s + this._transformMatrix[3] * c,
 				this._transformMatrix[4],
@@ -83,7 +94,7 @@
 		};
 	}
 	if (needPolyfill) {
-		Object.defineProperty(context.__proto__, 'mozCurrentTransform', {
+		Object.defineProperty(contextProto, 'mozCurrentTransform', {
 			get: function getCurrentTransform() {
 				return this._transformMatrix;
 			},
@@ -92,7 +103,7 @@
 		});
 	}
 	if (needPolyfillInverse) {
-		Object.defineProperty(context.__proto__, 'mozCurrentTransformInverse', {
+		Object.defineProperty(contextProto, 'mozCurrentTransformInverse', {
 			get: function getCurrentTransformInverse() {
 				var a = this._transformMatrix[0], b = this._transformMatrix[1],
 					c = this._transformMatrix[2], d = this._transformMatrix[3],
